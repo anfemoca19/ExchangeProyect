63b7c7f10c0346d2f0ad57d3a44cece78e04ca8c
Andres Morales <appandres993@gmail.com>
2023-09-06 11:52:15 -0500

    Se inicia el modal para la conexion de a wallet

---
+++ assets/js/market.js
@@ -0,0 +1,20 @@
+
+
+// COnexion a la api para hacer el listado de las criptos
+
+
+const API_KEY =  '850c7ee3-2f87-4d1c-86c5-4a7a84e48661'
+
+function api () {
+
+  fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=850c7ee3-2f87-4d1c-86c5-4a7a84e48661'
+    )
+  .then(res=>{console.log(res.json)})
+  .catch((err)=>{
+    console.log(err)
+  });
+
+}
+
+
+api();
